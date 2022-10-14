c6f0a4d6ff23ec18c2b9e5cb8edf137f1cba42da
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-10-14 11:09:23 -0500

    terminado taller 2

--- practicas_typescript/talleres-ts/taller-2/ej-1.ts
+++ practicas_typescript/talleres-ts/taller-2/ej-1.ts
@@ -61,19 +61,56 @@ console.log(modulo(15, 0.497));
 
 // ●investigue desestructuracion de objetos y de arreglos e implementelos.
 const persona = {
-  ojos: 'negros'
+  ojos: 'negros',
+  cara: 'ovalada',
+  altura: 194,
+  peso: 86,
+  contextura: 'delgado',
+  tes:'blanca'
 }
+
+const {ojos, cara, altura, peso, contextura, tes} = persona;
+
+console.log(`es de ojos ${ojos}, con una cara ${cara}, mide ${altura}cm, pesa ${peso} kg, es de contextura ${contextura} y de tes ${tes}`);
 
 const vehiculo = {
-
+  marca: 'toyota',
+  modelo: 'GR Yaris',
+  salida: 2021,
+  motor: '1.6 L turbo de 3 cilindros',
+  interior: 'deportivo'
 }
+
+const {marca, modelo, salida, motor, interior} = vehiculo;
+
+console.log(`el vehiculo es de la marca ${marca}, del modelo ${modelo}, salio en el año ${salida}, cuenta con un motor de ${motor} y el interior es ${interior}`);
 
 const edificio = {
-
+  color: 'gris',
+  ventanas: 'polarizadas',
+  pisos: 20,
+  año: 1984,
+  ocupacion: 'empresa de software'
 }
+
+const {color, ventanas, pisos, año, ocupacion} = edificio;
+
+console.log(`El edificio es de color ${color}, con ${pisos} pisos y ventanas ${ventanas}. fue construido en el año ${año} y actualmente lo ocupa una ${ocupacion}`);
 
 const enteros:Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+const [one, two, three, four, five, six, seven, eight, nine, ten] = enteros;
+
+console.log(`los numeros enteros son ${one}, ${two}, ${three}, ${four}, ${five}, ${six}, ${seven}, ${eight}, ${nine}, ${ten}`);
+
 const partesPC:Array<string> = ['monitor', 'tarjeta madre', 'gpu', 'cpu', 'fuente', 'ram', 'rom'];
 
-const comida:Array<string> = ['arroz', 'carne','lentejas', 'pollo', 'frijoles', 'platanos', 'bananos', 'pan'];
+const [first, second, third, fouth, fifth, sixth, seventh] = partesPC;
+
+console.log(`la PC cuenta con los siguentes componentes: ${first}, ${second}, ${third}, ${fouth}, ${fifth}, ${sixth}, ${seventh}`);
+
+const comida:Array<string> = ['arroz', 'carne','lentejas', 'pollo', 'frijoles', 'platanos']
+
+const [uno, dos , tres, cuatro, cinco, seis] =comida;
+
+console.log(`en la casa hay : ${uno}, ${dos} , ${tres}, ${cuatro}, ${cinco}, ${seis}`);
