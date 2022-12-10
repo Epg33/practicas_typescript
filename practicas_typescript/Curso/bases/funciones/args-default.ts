cab8e8f77e231790fa6a50c8cfa33c2660ae96d6
EthiemGuerrero <ethiemalex03@gmail.com>
2022-12-10 13:06:34 -0500

    default

---
+++ practicas_typescript/Curso/bases/funciones/args-default.ts
@@ -0,0 +1,15 @@
+(()=>{
+
+  const fullName = (firstName:string, lastName?:string, upper: boolean = false ):string =>{
+    if( upper ) {
+      return `${firstName} ${lastName || 'no lastname'}`.toUpperCase();
+    } else {
+      return `${firstName} ${lastName || 'no lastname'}`;
+    }
+    
+  }
+
+  const name = fullName('Tony', 'stark', true)
+  console.log({name});
+  
+})()
