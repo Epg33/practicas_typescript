376d2a9bb006107c7303236401ea29775d770f50
EthiemGuerrero <ethiemalex03@gmail.com>
2022-12-17 14:52:43 -0500

    private protected

---
+++ practicas_typescript/Curso/bases/classes/extends.ts
@@ -0,0 +1,34 @@
+(() => {
+  class Avenger {
+    constructor(
+      public name: string, 
+      public realName: string
+      ) {
+      console.log("Constructor avenger llamado");
+    }
+
+    protected getFullName() {
+      return `${this.name} ${this.realName}`;
+    }
+  }
+
+  class Xmen extends Avenger {
+    constructor(
+      name: string,
+      realName: string,
+      public isMutant: boolean
+      ){
+      super(name, realName);
+      console.log('Constructor Xmen Llamado')
+    }
+
+    getFullNameFromXmen(){
+      console.log(super.getFullName())
+    }
+  }
+
+  const wolverine = new Xmen("Wolverine", "Logan", true);
+
+  console.log(wolverine);
+  wolverine.getFullNameFromXmen()
+})();
--- practicas_typescript/Curso/bases/main.js
+++ practicas_typescript/Curso/bases/main.js
@@ -1,53 +1,45 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: "Samuel L. Jackson",
-        ironMan: "Robert Downey Jr.",
-        vision: "Paul Bettany",
-        activo: true,
-        poder: 1500.123154654,
-    };
-    const printAvenger = (_a) => {
-        var { ironMan } = _a, rest = __rest(_a, ["ironMan"]);
-        console.log(ironMan, rest);
-    };
-    const avengerArr = ['Cap. America', true, 150.15];
-    const [capi, ironMan,] = avengerArr;
-    console.log({ ironMan, capi });
-})();
-(() => {
-    const ironman = {
-        name: 'Ironman',
-        weapon: 'Armorsuit'
-    };
-    const capitanAmerica = {
-        name: 'Capitan America',
-        weapon: 'Escudo'
-    };
-    const thor = {
-        name: 'Thor',
-        weapon: 'Mjolnir'
-    };
-    const avengers = [ironman, thor, capitanAmerica];
-    for (const avenger of avengers) {
-        console.log(avenger.name, avenger.weapon);
+        static getAvgAge() {
+            return this.name;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
+    Avenger.avgAge = 35;
+    const antMan = new Avenger("AntMan", "Capitan", "Scott Lang");
+    console.log(antMan.bio());
 })();
 (() => {
-    const NOMBRE = 'Ethiem';
-    const getName = () => {
-        console.log('viejo getName');
-    };
+    class Avenger {
+        constructor(name, realName) {
+            this.name = name;
+            this.realName = realName;
+            console.log("Constructor avenger llamado");
+        }
+        getFullName() {
+            return `${this.name} ${this.realName}`;
+        }
+    }
+    class Xmen extends Avenger {
+        constructor(name, realName, isMutant) {
+            super(name, realName);
+            this.isMutant = isMutant;
+            console.log('Constructor Xmen Llamado');
+        }
+        getFullNameFromXmen() {
+            console.log(super.getFullName());
+        }
+    }
+    const wolverine = new Xmen("Wolverine", "Logan", true);
+    console.log(wolverine);
+    wolverine.getFullNameFromXmen();
 })();
 //# sourceMappingURL=main.js.map
